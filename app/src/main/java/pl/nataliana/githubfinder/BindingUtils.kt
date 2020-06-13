3e57c5da2ae8458ec00f110c9af04d424d5758e2
Natalia Nazaruk <nazarjazz@wp.pl>
2020-06-13 13:19:48 +0200

    Order made in bindings

--- app/src/main/java/pl/nataliana/githubfinder/BindingUtils.kt
+++ app/src/main/java/pl/nataliana/githubfinder/BindingUtils.kt
@@ -3,6 +3,7 @@ package pl.nataliana.githubfinder
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import pl.nataliana.githubfinder.model.GithubRepository
+import pl.nataliana.githubfinder.model.RepositoryCommits
 
 @BindingAdapter("textViewName")
 fun TextView.setTextViewName(item: GithubRepository?) {
@@ -15,5 +16,40 @@ fun TextView.setTextViewName(item: GithubRepository?) {
 fun TextView.setTextWineYear(item: GithubRepository?) {
     item?.let {
         text = item.repoName
+    }
+}
+
+@BindingAdapter("textViewNameDetail")
+fun TextView.setTextViewNameDetail(item: GithubRepository?) {
+    item?.let {
+        text = item.owner.userLogin
+    }
+}
+
+@BindingAdapter("textViewRepoDetail")
+fun TextView.setTextWineYearDetail(item: GithubRepository?) {
+    item?.let {
+        text = item.repoName
+    }
+}
+
+@BindingAdapter("commitMessage")
+fun TextView.setCommitMessage(item: RepositoryCommits?) {
+    item?.let {
+        text = item.commit.message
+    }
+}
+
+@BindingAdapter("commitSha")
+fun TextView.setCommitSha(item: RepositoryCommits?) {
+    item?.let {
+        text = item.commit.sha
+    }
+}
+
+@BindingAdapter("commitAuthor")
+fun TextView.setCommitAuthor(item: RepositoryCommits?) {
+    item?.let {
+        text = item.commit.author.name
     }
 }
