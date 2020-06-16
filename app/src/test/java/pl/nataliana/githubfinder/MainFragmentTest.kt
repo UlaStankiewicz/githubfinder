4e4b4c2a637c4b2055b24dbf00c6ac70fde0d456
Natalia Nazaruk <nazarjazz@wp.pl>
2020-06-16 20:39:17 +0200

    Setup for Robolectric and some small changes in main layout

---
+++ app/src/test/java/pl/nataliana/githubfinder/MainFragmentTest.kt
@@ -0,0 +1,9 @@
+package pl.nataliana.githubfinder
+
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class MainFragmentTest {
+
+}
